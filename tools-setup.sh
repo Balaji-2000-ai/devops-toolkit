fc79619302594575d31e8819b1a9d3c38a3c0483
Balaji-2000-ai <balaji167.k@gmail.com>
2025-07-29 12:22:05 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -4,6 +4,9 @@ git --version or git -v: Checks the installed Git version
 git init .: Initializes a Git repository in the current directory
 git status: Shows the current status of the working directory and staging area
 git add filename: Stages the file for commit
-git add file1 file2: Stages multiple files
+git add file1 file2: Stages multiple files 
 git add *: Stages all the files in the current directory
+git commit -m "message" .: Commits all staged files with the given commit message
+git commit -m "message" filename: Commits the staged changes of the specified file with the given commit message
+git commit -m "message" file file: Commits the staged changes of multiple files with the given commit message
 
