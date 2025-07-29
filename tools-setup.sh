8aad2cf55b660c5621353cba41c3fe891ae44a2d
Balaji-2000-ai <balaji167.k@gmail.com>
2025-07-29 11:34:18 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -1,1 +1,9 @@
-Git : yum install git -y
+#Git
+yum install git -y: Installs Git on the system
+git --version or git -v: Checks the installed Git version
+git init .: Initializes a Git repository in the current directory
+git status: Shows the current status of the working directory and staging area
+git add filename: Stages the file for commit
+git add file1 file2: Stages multiple files
+git add *: Stages all the files in the current directory
+
