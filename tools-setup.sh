11d80c17c2ef8797eec22d6d1c7e4271b6f25f49
Balaji-2000-ai <balaji167.k@gmail.com>
2025-07-29 13:39:50 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -9,7 +9,9 @@ git add *: Stages all the files in the current directory
 git commit -m "message" .: Commits all staged files with the given commit message
 git commit -m "message" filename: Commits the staged changes of the specified file with the given commit message
 git commit -m "message" file file: Commits the staged changes of multiple files with the given commit message
-git log : Shows detailed commit history with commit ID, author, date, and commit message
+git log : Shows detailed commit history with commit ID, author details, date, and commit message
 git show <commit-id>: Shows details of a specific commit
 git show : Shows details of the latest commit
+git config user.name "username" : Sets the Git author's username for commits (local repo)
+git config user.email "email@example.com" : Sets the Git author's email for commits (local repo)
 
