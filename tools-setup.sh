13401e94c1e4bea4d2cd8450375b2e8e8eb24b2b
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-10 15:34:53 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -1,3 +1,4 @@
 # Git
 yum install git -y: Installs Git on the system
 git --version or git -v: Checks the installed Git version
+yum install java-1.8.0-openjdk -y: Installs Java 8 (OpenJDK), required to run and build Java-based applications
