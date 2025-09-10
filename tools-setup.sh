8204fbc0691274fe41b339183c4b4f890d9bde29
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-10 15:55:46 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -10,4 +10,23 @@ yum install java-17-amazon-corretto -y: Installs Amazon Corretto 17 (OpenJDK 17 
 
 
 
+#Jenkins
+
+#STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
+yum install git java-1.8.0-openjdk maven -y
+
+#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+#STEP-3: DOWNLOAD JAVA17 AND JENKINS
+yum install java-17-amazon-corretto -y
+yum install jenkins -y
+
+#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
+systemctl start jenkins.service
+systemctl status jenkins.service
+
+
+
 
