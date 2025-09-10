15c503318ff9a843f4ddbc5baed1efcbd2a36a4c
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-10 15:38:33 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -2,4 +2,8 @@
 yum install git -y: Installs Git on the system
 git --version or git -v: Checks the installed Git version
 yum install java-1.8.0-openjdk -y: Installs Java 8 (OpenJDK), required to run and build Java-based applications
-java -version: Verifies Java installation and shows the installed version.
+java -version: Verifies Java installation and shows the installed version
+yum install maven -y: Installs Apache Maven, a build tool used to compile, package, and manage dependencies for Java-based applications
+mvn -v: Verifies Maven installation and shows the installed Maven version along with Java details
+
+
