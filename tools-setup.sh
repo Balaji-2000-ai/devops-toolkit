89166ff50cceca70be62f8eb2b0fd8c84f370127
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-12 14:58:44 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -35,6 +35,32 @@ systemctl start jenkins.service
 systemctl status jenkins.service
 
 
+#Tomcat
+
+#STEP-1: Install Java 17
+yum install java-17-amazon-corretto -y
+
+#STEP-2: Download Apache Tomcat 9.0.108
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
+
+#STEP-3: Extract Tomcat archive
+tar -zxvf apache-tomcat-9.0.108.tar.gz
+
+#STEP-4: Add user roles & credentials to tomcat-users.xml
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
+
+#STEP-5: Allow access to Tomcat Manager from any IP in context.xml
+sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+
+#STEP-6: Start Tomcat server
+sh apache-tomcat-9.0.108/bin/startup.sh
+
+
 
 
 
