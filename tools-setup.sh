13ccaac2cef3dd01c97bba83cef772c64d425603
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-12 16:13:50 +0100

    Update tools-setup.sh

--- tools-setup.sh
+++ tools-setup.sh
@@ -22,17 +22,22 @@ update-alternatives --config java: Switch between multiple installed Java versio
 #STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN 
 yum install git java-1.8.0-openjdk maven -y
 
-#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-#STEP-3: DOWNLOAD JAVA17 AND JENKINS
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade
 yum install java-17-amazon-corretto -y
 yum install jenkins -y
-
-#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl status jenkins.service
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
+mkdir -p /var/tmp_disk
+chmod 1777 /var/tmp_disk
+mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | tee -a /etc/fstab
+systemctl mask tmp.mount
+df -h /tmp
+systemctl restart jenkins
 
 
 #Tomcat
@@ -40,25 +45,25 @@ systemctl status jenkins.service
 #STEP-1: Install Java 17
 yum install java-17-amazon-corretto -y
 
-#STEP-2: Download Apache Tomcat 9.0.108
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
+#STEP-2: Download Apache Tomcat 9.0.109
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.109/bin/apache-tomcat-9.0.109.tar.gz
 
 #STEP-3: Extract Tomcat archive
-tar -zxvf apache-tomcat-9.0.108.tar.gz
+tar -zxvf apache-tomcat-9.0.109.tar.gz
 
 #STEP-4: Add user roles & credentials to tomcat-users.xml
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.108/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.109/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.109/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.109/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.109/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.109/conf/tomcat-users.xml
 
 #STEP-5: Allow access to Tomcat Manager from any IP in context.xml
-sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sed -i '21d' apache-tomcat-9.0.109/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.109/webapps/manager/META-INF/context.xml
 
 #STEP-6: Start Tomcat server
-sh apache-tomcat-9.0.108/bin/startup.sh
+sh apache-tomcat-9.0.109/bin/startup.sh
 
 
 
