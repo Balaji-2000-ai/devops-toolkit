b2bca1c10c8a340608e44a13974bbd0c90a0a843
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-17 11:10:00 +0100

    Update network_ports.sh

--- network_ports.sh
+++ network_ports.sh
@@ -1,4 +1,6 @@
 22- SSH //Securely connect to Linux servers for remote access
 3389- RDP //Remote Desktop Protocol for connecting to Windows servers
 80- HTTP //Access web applications over the internet (not secure)
-8080- Common alternative to HTTP (80); by default used to access Jenkins dashboard and other application servers like Tomcat
+8080- Used to access Jenkins dashboard and other application servers like Tomcat
+8081- Default port for Sonatype Nexus Repository Manager (artifact storage for builds)
+
