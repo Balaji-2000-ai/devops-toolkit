5f854de47830fdacfcb7ff6c08fb82365b72e74a
Balaji-2000-ai <balaji167.k@gmail.com>
2025-09-23 19:48:11 +0100

    Update network_ports.sh

--- network_ports.sh
+++ network_ports.sh
@@ -3,5 +3,6 @@
 80- HTTP //Access web applications over the internet (not secure)
 8080- Used to access Jenkins dashboard and other application servers like Tomcat
 8081- Default port for Sonatype Nexus Repository Manager (artifact storage for builds)
+9000- Default port for SonarQube web dashboard (code quality & security analysis)
 All Traffic- Allows all ports and protocols (0-65536)
 
