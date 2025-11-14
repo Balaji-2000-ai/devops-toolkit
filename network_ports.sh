00cc9d185f8e316c2601c00fddaac30243a99301
Balaji-2000-ai <balaji167.k@gmail.com>
2025-11-14 13:11:20 +0000

    Update network_ports.sh

--- network_ports.sh
+++ network_ports.sh
@@ -4,6 +4,7 @@
 8080- Used to access Jenkins dashboard and other application servers like Tomcat
 8081- Default port for Sonatype Nexus Repository Manager (artifact storage for builds)
 9000- Default port for SonarQube web dashboard (code quality & security analysis)
+9000- Commonly used by tools like Docker Portainer
 All Traffic- Allows all ports and protocols (0-65536)
 3306- The default port used by MySQL for database connections
 
